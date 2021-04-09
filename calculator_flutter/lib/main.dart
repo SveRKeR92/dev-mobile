c3a5fb9090ce5c5fb261f50edbade2fee77cf527
Maxime <maxime.attala@edu.devinci.fr>
2021-04-09 19:21:53 +0200

    dart calculator, need to finish operators

--- calculator_flutter/lib/main.dart
+++ calculator_flutter/lib/main.dart
@@ -1,113 +1,158 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color displayContainerBackground = Color(0xFF3E606F);
+  static const Color inputContainerBackground = Color(0xFF193441);
+}
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calculatrice',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: AppColors.inputContainerBackground,
+        accentColor: AppColors.displayContainerBackground,
+        primaryColorBrightness: Brightness.light,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Calculator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String? title;
-
+class Calculator extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _CalculatorState createState() => _CalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CalculatorState extends State<Calculator> {
+  static const List<List<String>> grid = <List<String>>[
+    <String>["7", "8", "9", "-"],
+    <String>["4", "5", "6", "*"],
+    <String>["1", "2", "3", "/"],
+    <String>["0", ".", "=", "+"],
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  double? input;
+  double? previousInput;
+  String? symbol;
+
+  void onItemClicked(String value) {
+    print('On Click $value');
+
+    switch (value) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        onNewDigit(value);
+        break;
+      case '+':
+      case '-':
+      case '/':
+      case '*':
+        onNewSymbol(value);
+        break;
+      case '=':
+        onEquals(value);
+    }
+
+    // Force l'interface à se redessiner
+    setState(() {});
+  }
+
+  void onNewDigit(String digit) {
+    input = double.parse(digit);
+    previousInput = input;
+  }
+
+  void onNewSymbol(String digit) {
+    symbol = digit;
+  }
+
+  void onEquals(String digit) {
+    input = previousInput? + symbol + double.parse(digit);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title!),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: DefaultTextStyle(
+        style: TextStyle(color: AppColors.white, fontSize: 22.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Flexible(
+              flex: 2,
+              child: Container(
+                color: AppColors.displayContainerBackground,
+                alignment: AlignmentDirectional.centerEnd,
+                padding: const EdgeInsets.all(22.0),
+                child: Text(input?.toString() ?? "0"),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Flexible(
+              flex: 8,
+              child: Column(
+                // Pour chaque nouvelle ligne, on itère sur chaque cellule
+                children: grid.map((List<String> line) {
+                  return Expanded(
+                    child: Row(
+                        children: line
+                            .map(
+                              (String cell) => Expanded(
+                                child: InputButton(
+                                  label: cell,
+                                  onTap: onItemClicked,
+                                ),
+                              ),
+                            )
+                            .toList(growable: false)),
+                  );
+                }).toList(growable: false),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class InputButton extends StatelessWidget {
+  final String label;
+  final ValueChanged<String>? onTap;
+
+  InputButton({required this.label, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => onTap?.call(label),
+      child: Ink(
+        height: double.infinity,
+        decoration: BoxDecoration(
+            border: Border.all(color: AppColors.white, width: 0.5),
+            color: AppColors.inputContainerBackground),
+        child: Center(
+          child: Text(
+            label,
+          ),
+        ),
+      ),
     );
   }
 }
